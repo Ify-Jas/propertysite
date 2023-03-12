84c1910b1b15c254ae8fd41961bb13032bef4e5e
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-03-12 20:28:07 +0000

    updating codes for project.js and image for team.js

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,18 +1,25 @@
 import jsonteam from '../teamprofile.json';
+import Header from "../components/header";
+import pic1 from '../images/logofinal.png'
+import pic2 from '../images/Profilepic.jpg';
+
+
 
 function Team(){
     const team = jsonteam.teamprofile;
     return(
-        <div>
+        <div className='profileContainer'>
+            <Header />
                            
                 {team.map((team, index)=>(
                     <div key={index} className='profileCard'>
-                        <main className='profileImage'>
-                            <img src={team.image} alt='profile' />
+                        <main className='profileImageContainer'>
+                            <img className='profileImage' src='https://api.lorem.space/image/fashion?w=150&h=150' alt='profile' />
                         </main>
                         <div className='profile'>
                             <h1>{team.name}</h1>
                             <h6>{team.position}</h6>
+                            <div className='profileLine'></div>
                             <p>{team.profile}</p>
                         </div>
 
