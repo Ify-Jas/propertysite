e1df44bba8a4251f078105f9609476d228c6fe90
Ifeoma Ezeoke <ifyezeoke@gmail.com>
2023-03-16 22:37:20 +0000

    updating with codes for companyjs, services.js and post api to send message

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,6 +1,69 @@
 import Header from "../components/header";
+import { useState } from 'react';
+import Button from 'react-bootstrap/Button';
+import Modal from 'react-bootstrap/Modal';
 
 function Contact(){
+    const [contactData, setContactData] = useState({
+        name: '',
+        email: '',
+        subject: '',
+        message: '',
+        access_key: 'd574540e-45f0-4a98-a6c0-16b1d6568819'
+    });
+    const handleChange = (event) => {
+        setContactData({
+            ...contactData,
+            [event.target.name]: event.target.value,
+        });
+    }
+    const handleClick = (event) => {
+        event.preventDefault();
+        if(!contactData.name.length || !contactData.email.length){
+            return
+            <div
+            className="modal show"
+            style={{ display: 'block', position: 'initial' }}
+          >
+            <Modal.Dialog>
+              <Modal.Header closeButton>
+                <Modal.Title>Alert!!</Modal.Title>
+              </Modal.Header>
+      
+              <Modal.Body>
+                <p>Please enter your name or email.</p>
+              </Modal.Body>
+      
+              <Modal.Footer>
+                <Button variant="secondary">Close</Button>
+              </Modal.Footer>
+            </Modal.Dialog>
+          </div>
+        } else {
+           const data = JSON.stringify(contactData);
+           fetch("https://api.web3forms.com/submit", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+              Accept: "application/json",
+            },
+            body: data,
+          })
+          .then(res => res.json())
+          .then((data)=> {
+            console.log(data);
+            setContactData({
+                ...contactData,
+                name: "",
+                email: "",
+                subject: '',
+                message: "",
+              });
+          })
+
+        }
+
+    }
     return(
         <div className="mainCard" >
             <main>
@@ -10,10 +73,25 @@ function Contact(){
                 <form className="contactCard">
                     <h1>Contact Form</h1>
 
-                    <input type='text' placeholder="Enter your name" />
-                    <input type='text' placeholder="Enter your email" />
+                    <input 
+                    type='text' 
+                    placeholder="Enter your name"
+                    name='name'
+                    value={contactData.name}
+                    onChange={handleChange} />
+
+                    <input 
+                    type='text'
+                    name='email'
+                    value={contactData.email}
+                    placeholder="Enter your email"
+                    onChange={handleChange} />
                     <label>Choose a subject</label>
-                    <select>
+
+                    <select 
+                    onChange={handleChange}
+                    name='subject'
+                    value={contactData.subject}>
                         <option>Buying and selling enquiry</option>
                         <option>Rental including shortlet</option>
                         <option>Renovation</option>
@@ -23,8 +101,18 @@ function Contact(){
                         <option>Other</option>
 
                     </select>
-                    <textarea className="message" placeholder="Type your message here"></textarea>
-                    <button className="contactBtn">Send</button>
+
+                    <textarea 
+                    className="message" 
+                    placeholder="Type your message here"
+                    name="message"
+                    value={contactData.message}
+                    onChange={handleChange}>
+                    </textarea>
+
+                    <button 
+                    className="contactBtn"
+                    onClick={handleClick}>Send</button>
                 </form>
             </div>
           
--- src/pages/services.js
+++ src/pages/services.js
@@ -1,7 +1,26 @@
+import Header from "../components/header";
+
 function Services(){
     return(
-        <>
-        </>
+        <div>
+            <Header />
+            <div className='aboutContainer'>
+            <main className='aboutCompany'>
+                <h1>Buying and Selling</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus sed turpis. Habitasse platea dictumst vestibulum rhoncus. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Nibh praesent tristique magna sit amet purus gravida quis blandit. Vitae suscipit tellus mauris a. Volutpat ac tincidunt vitae semper quis. Quam pellentesque nec nam aliquam sem et tortor consequat id. Vel elit scelerisque mauris pellentesque. Volutpat blandit aliquam etiam erat. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Leo vel fringilla est ullamcorper. Tortor id aliquet lectus proin nibh. Ut porttitor leo a diam. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Blandit volutpat maecenas volutpat blandit aliquam etiam. Ut tellus elementum sagittis vitae et.</p>
+            </main>
+            <section className='aboutCompany'>
+                <h1>Rental Services</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus sed turpis. Habitasse platea dictumst vestibulum rhoncus. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Nibh praesent tristique magna sit amet purus gravida quis blandit. Vitae suscipit tellus mauris a. Volutpat ac tincidunt vitae semper quis. Quam pellentesque nec nam aliquam sem et tortor consequat id. Vel elit scelerisque mauris pellentesque. Volutpat blandit aliquam etiam erat. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Leo vel fringilla est ullamcorper. Tortor id aliquet lectus proin nibh. Ut porttitor leo a diam. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Blandit volutpat maecenas volutpat blandit aliquam etiam. Ut tellus elementum sagittis vitae et.</p>
+            </section>
+            <section className='aboutCompany'>
+                <h1>Property Development</h1>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Massa placerat duis ultricies lacus sed turpis. Habitasse platea dictumst vestibulum rhoncus. Ornare quam viverra orci sagittis eu volutpat odio facilisis. Nibh praesent tristique magna sit amet purus gravida quis blandit. Vitae suscipit tellus mauris a. Volutpat ac tincidunt vitae semper quis. Quam pellentesque nec nam aliquam sem et tortor consequat id. Vel elit scelerisque mauris pellentesque. Volutpat blandit aliquam etiam erat. Viverra mauris in aliquam sem fringilla ut morbi tincidunt augue. Leo vel fringilla est ullamcorper. Tortor id aliquet lectus proin nibh. Ut porttitor leo a diam. Commodo odio aenean sed adipiscing diam donec adipiscing tristique risus. Blandit volutpat maecenas volutpat blandit aliquam etiam. Ut tellus elementum sagittis vitae et.</p>
+            </section>
+
+            </div>
+        
+        </div>
     )
 }
 export default Services;
